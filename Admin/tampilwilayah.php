3c91919e84713cab9d130584770512a9e93cf1ab
Yunita Maulinda <108499355+yunitamaulindasp@users.noreply.github.com>
2022-07-01 15:32:53 +0700

    Create tampilwilayah.php

---
+++ Admin/tampilwilayah.php
@@ -0,0 +1,84 @@
+<nav aria-label="breadcrumb" role="navigation">
+	<ol class="breadcrumb adminx-page-breadcrumb">
+		<li class="breadcrumb-item"><a href="?kode=dasboard">Home</a></li>
+		<li class="breadcrumb-item active" aria-current="page">Data Wilayah</li>
+		<li class="breadcrumb-item active" aria-current="page">Daftar Wilayah Kota Batu</li>
+	</ol>
+</nav>
+						
+<div class="pb-3">
+	<h1>Daftar Wilayah Kota Batu</h1>
+</div>
+						
+<div class="row">
+	<div class="col">
+		<div class="card mb-grid">
+			<div class="table-responsive-md">
+				<div class="card-body">
+					<table id="tabel" class="table table-bordered" data-table>
+					<style>
+						th,td { text-align: center; }
+					</style>
+						<thead>
+							<tr>
+								<th width="50px">No</th>
+								<th width="400px">Kecamatan</th>
+								<th width="300px">Kelurahan/Desa</th>
+								<th width="200px">Rincian</th>					
+							</tr> 
+						</thead>				
+						<tbody>											
+						</tbody>
+					</table>
+				</div>				
+			</div>
+		</div>
+	</div>					
+</div>
+
+<script type="text/javascript">
+	function KonfirmasiHapus(x)
+	{	bootbox.confirm({
+			title: "Konfirmasi",
+			message: "ID Desa " + x + " dihapus?",
+			callback: function(result){
+				if (result == true)
+				{	$.ajax({
+						type: "GET",
+						url: "hapuswilayah.php",
+						data: { "ID" : x},
+				})
+				.done(function(hasilProses){
+					bootbox.alert({
+						title: "Hasil Penghapusan",
+						message: hasilProses,
+						callback: function(result){
+							window.location.href = "default.php?kode=tampilwilayah";
+						}
+					});
+				})
+				.fail(function(jqXHR, textStatus){
+					alert( "Request gagal: " + textStatus );
+				});
+				}
+			}
+	});
+	}
+	$(document).ready(function(){
+		var dataTable = $('#tabel').DataTable( {
+			"processing": true,
+			"serverSide": true,
+			"bSort": false,
+			"ajax": {
+				url: "prosestampilwilayah.php",
+				type: "POST",
+				error: function(){
+					$(".lookup-error").html("");
+					$("#tabel").append('<tbody class="employee-grid-error"> <tr> <th colspan="3"> Data Tidak Ditemukan </th> </tr> </tbody>');
+					$("#lookup_processing").css("display", "none");
+				}
+			}
+			  
+		});
+	});
+</script>
