8c3c4b647ac36513e7e7488be4e04920295f56aa
Yunita Maulinda <108499355+yunitamaulindasp@users.noreply.github.com>
2022-07-02 19:00:26 +0700

    Create home.php

---
+++ Pelanggan/home.php
@@ -0,0 +1,26 @@
+<div class="slider-area">
+	<div class="single-slider slider-height d-flex align-items-center">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-xl-8 col-lg-9">
+					<div class="hero__caption">
+						<h1>Cek Tagihan Airmu Disini!</h1>
+						<p>Cek tagihan air sekarang lebih mudah tanpa harus kekantor PDAM!</p>
+						<p>Pembayaran tagihan air juga kini lebih mudah dengan membayar melalui ATM atau swalayan terdekat!</p>
+					</div>
+					<form action="?kode=pop" method="POST" class="search-box">
+						<div class="input-form">
+							<input id="saluran" name="saluran" type="text" placeholder="Masukkan ID Saluran Anda">
+							<div class="search-form">
+								<button><i class="ti-angle-double-right"></i></button>
+							</div>
+							<div class="world-form">
+								<i class="ti-unlock"></i>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
