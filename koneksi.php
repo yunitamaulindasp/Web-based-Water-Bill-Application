aa47cc573ff6eaecdef0787af4a4f2ba64065906
Yunita Maulinda <108499355+yunitamaulindasp@users.noreply.github.com>
2022-07-02 22:37:43 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -3,10 +3,8 @@
 	$user = "root";
 	$password = "";
 	$database = "pdam";
-	// Buat koneksi
 	$mysqli = new mysqli($server, $user, $password, $database);
 	$koneksi = mysqli_connect($server, $user, $password, $database);
-	// Periksa koneksi
 	if ($mysqli->connect_error)
 	{ die("Koneksi gagal: " . $mysqli->connect_error);
 	}
